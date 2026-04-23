f9591ef77fdc1258751a9738100e758013beb9ec
Максим Сагеев <gr636_samvi@lab-308-7.tomtit-tomsk.ru>
2026-04-23 14:27:16 +0700

    Cleaning code before merging

--- MIAPO_project/main.py
+++ MIAPO_project/main.py
@@ -1,10 +1,1 @@
-def get_player_choice():
-    while True:
-        player_choice = input('Enter Rock, Paper or Scissor.\n')
-        if player_choice in ("Rock", "Paper", "Scissor"):
-            return player_choice
-        else:
-            print('Please, enter allowed value!')
-
-
-
+placeholder :(
