2a253b411ff4e8ee74fd52f7b7d67ea19f3b6fb3
Максим Сагеев <gr636_samvi@lab-308-7.tomtit-tomsk.ru>
2026-04-23 14:35:48 +0700

    Preparing for merging

--- MIAPO_project/get_and_display_rules.py
+++
@@ -1,5 +0,0 @@
-def read_file():
-    with open('readme.txt', 'r', encoding= 'utf-8') as file:
-        content = file.read()
-    print(content)
-read_file()
--- MIAPO_project/main.py
+++ MIAPO_project/main.py
@@ -1,1 +1,4 @@
-placeholder :(
+def read_file():
+    with open('readme.txt', 'r', encoding= 'utf-8') as file:
+        content = file.read()
+    print(content)
