2a5a53ee98b47b34a8874e1df6d3d9a756c362ec
Максим Сагеев <gr636_samvi@lab-308-7.tomtit-tomsk.ru>
2026-04-23 15:27:11 +0700

    Comleted merge

--- MIAPO_project/main.py
+++ MIAPO_project/main.py
@@ -1,4 +1,9 @@
-<<<<<<< HEAD
+import random
+def get_computer_choice():
+    computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
+    return computer_choice
+
+
 def get_player_choice():
     while True:
         player_choice = input('Enter Rock, Paper or Scissors.\n')
@@ -6,12 +11,6 @@ def get_player_choice():
             return player_choice
         else:
             print('Please, enter allowed value!')
-
-
-import random
-def get_computer_choice():
-    computer_choice = random.choice(['Rock', 'Paper', 'Scissors'])
-    return computer_choice
 
 
 def determine_winner(player_choice, computer_choice):
@@ -28,3 +27,22 @@ def read_file():
         content = file.read()
     print(content)
 
+
+while True:
+    print('1 - Start game\n2 - Show rules')
+    user_input = input()
+    if user_input == '1':
+        player_choice = get_player_choice()
+        computer_choice = get_computer_choice()
+        result = determine_winner(player_choice, computer_choice)
+        print(computer_choice)
+        if result == 'draw':
+            print('draw')
+        if result == True:
+            print('win')
+        if result == False:
+            print('lose')
+    elif user_input == '2':
+        read_file()
+    else:
+        print('Invalid input!')
