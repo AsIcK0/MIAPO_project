d04b83dd3a88ae180cb717e56fc142dd743a59de
Максим Сагеев <gr636_samvi@lab-308-7.tomtit-tomsk.ru>
2026-04-23 14:33:40 +0700

    Preparing for merging

--- MIAPO_project/determine_winner.py
+++
@@ -1,7 +0,0 @@
-def determine_winner(player_choice, computer_choice):
-    if player_choice == computer_choice:
-        return 'draw'
-    if (player_choice == 'Rock' and computer_choice == 'Scissors') or (player_choice == 'Paper' and computer_choice == 'Rock' ) or (player_choice == 'Scissors' and computer_choice == 'Paper'):
-        return True
-    else:
-        return False
--- MIAPO_project/main.py
+++ MIAPO_project/main.py
@@ -1,1 +1,7 @@
-placeholder :(
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        return 'draw'
+    if (player_choice == 'Rock' and computer_choice == 'Scissors') or (player_choice == 'Paper' and computer_choice == 'Rock' ) or (player_choice == 'Scissors' and computer_choice == 'Paper'):
+        return True
+    else:
+        return False
